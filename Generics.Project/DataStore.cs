f7f498b46d4b244aa07a59fcd4137138f37bd92c
Bruno Ferreira Rocha <bruno_ferreira@hotmail.it>
2022-04-27 15:17:27 +0200

    Add project files.

---
+++ Generics.Project/DataStore.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics.Project
+{
+    public class DataStore<T> where T : class, new()
+    {
+        static int index = 10;
+        T[] _data = new T[index]; // 
+        T entry = new();
+        Dictionary<string, T> _dataList = new Dictionary<string, T>();
+        public DataStore()
+        {
+
+        }
+
+        public void AddItem(T item)
+        {
+            if (_data[_data.Length - 1] == null) //_> Se ho spazio
+            {
+                var element = Array.IndexOf(_data,
+                    Array.Find(_data, x => x == null));
+                _data[element] = item;
+            }
+            else
+            {
+                T[] _nextData = new T[_data.Length + 4];
+                Array.Copy(_data, _nextData, index);
+                _data = _nextData;
+            }
+        }
+        public T RetriveFromDictionary(string key)
+        {
+            if (_dataList is not null)
+            {
+                T valore = null;
+                _dataList.TryGetValue(key, out valore);
+                return valore;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        // Add
+        // Get
+        // Print
+        public void Print()
+        {
+            var campi = entry.GetType().GetProperties();
+
+            foreach (var prop in campi)
+            {
+                Console.WriteLine(prop.Name);
+            }
+        }
+    }
+}
---
+++ Generics.Project/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Generics.Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public int Age { get; set; }
+        public Student()
+        {
+
+        }
+    }
+}
